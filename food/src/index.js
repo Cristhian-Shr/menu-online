197e26bbf03486215717ed42a3046a14801ccc0b
Cristhian-shr <81269993+Cristhian-shr@users.noreply.github.com>
2024-02-02 10:05:02 -0300

    add codificacao home, index, produto-vitrine

--- food/src/index.js
+++ food/src/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Saudacao from "../src/components/saudacao/saudacao.jsx";
 import "./style/global.css";
-import Home from "./pages/home/home.jsx";
+import Rotas from "./rotas.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div className="saudacao-titulo">
-    <Home />
+    <Rotas />
   </div>
 );
--- food/src/produto-vitrine/produto-vitrine.jsx
+++ food/src/produto-vitrine/produto-vitrine.jsx
@@ -1,7 +1,26 @@
 import "./produto-vitrine.css";
+import foto from "../assets/hamburguer.png";
+import bag from "../assets/bag-black.png";
 
-function ProdutoVitrine() {
-  return <div className="produto-box text-center">produto</div>;
+function ProdutoVitrine(props) {
+  return (
+    <div className="produto-box text-center">
+      <img src={foto} alt="foto" />
+      <div>
+        <h2 className="titulo">Spicy Burguer</h2>
+        <p className="prod-vitrine-descricao">
+          Hamburguer de 250g, queijo, tomate, alface e cebola
+        </p>
+        <p className="prod-vitrine-preco">R$ 18,90</p>
+      </div>
+      <div>
+        <button className="btn btn-cart">
+          <img src={bag} className="icon" />
+          Adicionar
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default ProdutoVitrine;
